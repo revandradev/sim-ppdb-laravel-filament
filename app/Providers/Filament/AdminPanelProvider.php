ee7efe519cd089d49ae1138cfeae5187438032bf
duwi haryanto <haryanto@uii.ac.id>
2025-10-04 15:22:05 +0700

    set admin profile for the admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->profile()
+            ->profile('admin')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
