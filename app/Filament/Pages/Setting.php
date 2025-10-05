5041995d7534c040ba23c741be8fb9a80d94502a
duwi haryanto <haryanto@uii.ac.id>
2025-10-05 20:43:47 +0700

    Enhance Setting page: add site logo upload and improve input labels

--- app/Filament/Pages/Setting.php
+++ app/Filament/Pages/Setting.php
@@ -3,12 +3,14 @@ namespace App\Filament\Pages;
 
 use App\Settings\GeneralSetting;
 use BackedEnum;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\SettingsPage;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 use UnitEnum;
 
 class Setting extends SettingsPage
@@ -28,9 +30,22 @@ class Setting extends SettingsPage
                     ->description('Ubah nama dan deskripsi situs sesuai kebutuhan aplikasi Anda.')
                     ->aside()
                     ->schema([
-                        TextInput::make('site_name')
+                        TextInput::make('site_name')->label('Nama Situs')
                             ->required(),
-                        Textarea::make('site_description')->autosize()->rows(5)
+                        FileUpload::make('site_logo')
+                            ->label('Logo Situs')
+                            ->image()
+                            ->directory('site-logos')
+                            ->maxSize(1024) // Maksimum ukuran file dalam KB
+                            ->nullable()
+                            ->visibility('public')
+                            ->getUploadedFileNameForStorageUsing(
+                                fn(TemporaryUploadedFile $file): string => (string) str($file->getClientOriginalName())
+                                    ->prepend('site-logo.'),
+                            ),
+                        Textarea::make('site_description')
+                            ->label('Deskripsi Situs')
+                            ->autosize()->rows(5)
                             ->required(),
 
                     ])->columnSpanFull()
