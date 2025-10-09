614b035a8c4f88ad7e3a6447ce28697c0cff6201
duwi haryanto <haryanto@uii.ac.id>
2025-10-09 19:32:53 +0700

    feat: Add TahunMasuk resource with management pages, form, and table configurations

---
+++ app/Filament/Resources/TahunMasuks/Pages/ManageTahunMasuks.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\TahunMasuks\Pages;
+
+use App\Filament\Resources\TahunMasuks\TahunMasukResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageTahunMasuks extends ManageRecords
+{
+    protected static string $resource = TahunMasukResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
+}
